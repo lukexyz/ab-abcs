c4809a04c5c5f7dc5643b006908a962f68770e0b
lukexyz <lukedwoods@gmail.com>
2020-10-25 12:12:48 +0000

    add state variables

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ import pandas as pd
 import altair as alt
 from altair import Chart, X, Y, Axis, SortField, OpacityValue
 
+# 2020-10-25 edit@ from st.annotated_text import annotated_text
+from annotated_text import annotated_text  
+
+import st_state
+
 
 def main():
     st.beta_set_page_config(
@@ -15,21 +20,55 @@ def main():
         layout="centered",             # Can be "centered" or "wide". In the future also "dashboard", etc.
         initial_sidebar_state="auto")  # Can be "auto", "expanded", "collapsed"
 
+
+
     # ======================= Nav Bar  ==================== #
-    nav = 2
+    nav = 1
     part1, part2, part3 = st.beta_columns([1, 1, 1])
-    with part1:  # excuse hacky whitespace (alt+255) alignment 
-        if st.button('💠 Part I: Probability          '):      nav = 1
+
+    pages = ['💠 Part I: Probability          ', 
+             '💠 Part II: Error                    ',
+             '💠 Part III: P-values             ']
+
+    pages[nav] = pages[nav]+' Selected'
+
+    with part1:  # excuse hacky whitespace (alt+255) for alignment 
+        if st.button(pages[0]): nav = 0
     with part2:
-        if st.button('💠 Part II: Error                    '): nav = 2
+        if st.button(pages[1]): nav = 1
     with part3:
-        if st.button('💠 Part III: P-values             '):    nav = 3
+        if st.button(pages[2]): nav = 2
     st.markdown('---')
 
-    if nav == 1:
+    if nav == 0:
         st.text('part I')
+        
+        state = st_state._get_state()
+        pages = {
+            "Dashboard": st_state.page_dashboard,
+            "Settings": st_state.page_settings,
+        }
 
-    elif nav == 2: ######## PART II ############
+        st.sidebar.title(":floppy_disk: Page states")
+        page = st.sidebar.radio("Select your page", tuple(pages.keys()))
+
+        # Display the selected page with the session state
+        pages[page](state)
+
+        annotated_text("This ",
+            ("is", "verb", "#8ef"),
+            " some ",
+            ("annotated", "adj", "#faa"),
+            ("text", "noun", "#afa"),
+            " for those of ",
+            ("you", "pronoun", "#fea"),
+            " who ",
+            ("like", "verb", "#8ef"),
+            " this sort of ",
+            ("thing", "noun", "#afa"))
+
+
+    elif nav == 1: ######## PART II ############
 
         # ================== AB Test Sliders  ================== #
         col1, col2 = st.beta_columns([1, 1]) # first column 4x the size of second
@@ -118,6 +157,10 @@ def main():
     
     elif nav == 3: ######## PART III ############    
         st.text('part 3')
+
+
+    # Mandatory to avoid rollbacks with widgets, must be called at the end of your app
+    state.sync()
 
 if __name__ == '__main__':
     main()
